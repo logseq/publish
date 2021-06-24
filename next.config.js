d1223b5f2e52678bc480edc4a84d1d0af43bf729
Junyu Zhan <thezjy@gmail.com>
2021-06-24 23:45:32 +0800

    Redirect / to /pages/Contents

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   transpileModules: ['unist-util-visit', 'unist-util-is'],
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pages/Contents',
+        permanent: true,
+      },
+    ]
+  },
 }
